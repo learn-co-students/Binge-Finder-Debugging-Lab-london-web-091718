31b80369cbb1642179eaf8fb4f850769fd1295ea
Irfan Suleman <ivsuleman@gmail.com>
2018-11-27 18:53:58 +0000

    Done.

--- src/Adapter.js
+++ src/Adapter.js
@@ -1,8 +1,12 @@
-class Adapter {
-  static getShows (){
-    fetch("http://api.tvmaze.com/shows")
-    .then(res => res.json())
+import React from 'react'
+
+class Adapter extends React.Component {
+
+  static getShows() {
+    return fetch("http://api.tvmaze.com/shows")
+      .then(res => res.json())
   }
+
 
   // static getShowEpisodes (showID){
   //   return fetch(`http://api.tvmaze.com/shows/${showID}/episodes`)
